01af3bd8a1b8e8a4e7d7955606b02905bbca25e8
Ben <daprogramerforbatch@gmail.com>
2017-08-20 12:35:45 -0700

    Don't time out, also I can be at least a little neat!
    Some hosting will disable set_time_limit, if so just remeber to enter
    your email as the script will still download :+1:

--- index.php
+++ index.php
@@ -5,12 +5,10 @@
 if(!file_exists("downloads")){mkdir ("downloads");}
 $bf= disk_free_space("downloads/");
 $bt = disk_total_space("/");
-function HumanSize($Bytes)
-{
+function HumanSize($Bytes){
   $Type=array("", "kilo", "mega", "giga", "tera", "peta", "exa", "zetta", "yotta");
   $Index=0;
-  while($Bytes>=1024)
-  {
+  while($Bytes>=1024)  {
     $Bytes/=1024;
     $Index++;
   }
@@ -20,8 +18,8 @@ HumanSize($bf);
 ?>
 
 <form action="cdownload.php" method="post">
-URL to download: <input type="text" name="url"><br>
-My email (to notify, optional): <input type="text" name="email"><br>
+	URL to download: <input type="text" name="url"><br>
+	My email (to notify, optional): <input type="text" name="email"><br>
 <input type="submit">
 </form>
 
