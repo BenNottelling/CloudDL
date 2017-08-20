d9262a19242283a52b697970227477eb2ee5f928
Ben <daprogramerforbatch@gmail.com>
2017-08-20 12:56:41 -0700

    Fixed (should have been the first time)
    Can now download files over cURL, but it is limited to the maximum ram
    on the server

--- cdownload.php
+++ cdownload.php
@@ -11,6 +11,7 @@ curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
 curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 0);
 curl_setopt($ch, CURLOPT_MAXREDIRS, 3);
 curl_setopt($ch, CURLOPT_TIMEOUT, 0);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 $dl = curl_exec($ch);
 $filename = basename($url);
 echo "Downloading $url "; 
@@ -23,7 +24,7 @@ echo "<a href='$downloadedto'>$downloadedto</a>";
 
 
 
-file_put_contents("downloads/$filename", $dl, 'r', LOCK_EX);
+file_put_contents("downloads/$filename", $dl, LOCK_EX);
 If(!empty($email)){
 mail ($email, "Your cloud download is complete!", "Good news! Your cloud download of $filename is finished!, please download it soon before it is auto deleted from our server $downloadedto");
 }
--- rmall.php
+++ rmall.php
@@ -3,5 +3,6 @@ $files = glob('downloads/*'); // get all file names
 foreach($files as $file){ // iterate files
   if(is_file($file))
     unlink($file); // delete file
+	echo "Detleted $file";
 }
 ?>
