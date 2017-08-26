a3696ee8bd6897f76dc96eba97c340249057183e
Ben <daprogramerforbatch@gmail.com>
2017-08-26 16:39:28 -0700

    Renamed $domain so it doesn't mess with the download link, let's correct the filename

--- shareurl.php
+++ shareurl.php
@@ -1,8 +1,9 @@
 <?php
-$domain = str_replace ('www.', '', parse_url($url, PHP_URL_HOST)); //Let's find what domain this is.
+$downdomain = str_replace ('www.', '', parse_url($url, PHP_URL_HOST)); //Let's find what domain this is.
 
-if ($domain = "dropbox.com") { //If it's dropbox we want it to end with "?dl=1" for auto downloading.
+if ($downdomain = "dropbox.com") { //If it's dropbox we want it to end with "?dl=1" for auto downloading.
 	$url = str_replace (['?dl=0','?dl=1', '?'], '', $url);
 	$url = $url . '?dl=1';
+	$filename = str_replace (['?dl=0','?dl=1', '?'], '', $filename);
 }
 ?>
