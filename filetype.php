bbca7b505564f65fb7e75e8467fade2437bedea5
Ben <daprogramerforbatch@gmail.com>
2017-09-03 13:37:00 -0700

    Detect ISO files, don't add an extra '.' if the filetype is unknown

--- filetype.php
+++ filetype.php
@@ -2,13 +2,14 @@
 $mime = mime_content_type("downloads/$filename");
 $mime_types = array(//mime_type then extension, feel free to update it from here http://svn.apache.org/repos/asf/httpd/httpd/trunk/docs/conf/mime.types
 	//Archives
-	'application/zip' => 'zip',
-	'application/x-rar-compressed' => 'rar',
-	'application/vnd.ms-cab-compressed' => 'cab',
-	'application/java-archive' => 'jar',
-	'application/vnd.android.package-archive' => 'apk',
-	'application/x-7z-compressed' => '7z',
-	'application/x-tar' => 'tar',
+	'application/zip' => '.zip',
+	'application/x-rar-compressed' => '.rar',
+	'application/vnd.ms-cab-compressed' => '.cab',
+	'application/java-archive' => '.jar',
+	'application/vnd.android.package-archive' => '.apk',
+	'application/x-7z-compressed' => '.7z',
+	'application/x-tar' => '.tar',
+	'application/x-iso9660-image' => '.iso',
 );
-$newfilename = $filename . '.' . $mime_types[$mime];
+$newfilename = $filename . $mime_types[$mime];
 ?>
