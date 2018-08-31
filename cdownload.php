e994049bda1eec4ea37ea5c801833868adeae740
Ben <daprogramerforbatch@gmail.com>
2018-08-31 09:50:46 -0700

    Output what's going on

--- cdownload.php
+++ cdownload.php
@@ -9,17 +9,29 @@ $filename = basename($url);
 include "shareurl.php"; //for shared URLs (like from dropbox)
 
 
-$filename = str_replace ([' ','%', '?', '[' , ']', '-','&', '^', '*' , '/'], '', $filename);
-file_put_contents("downloads/$filename", fopen("$url", 'r'), LOCK_EX);
-include 'filetype.php';
-rename("downloads/$filename", "downloads/$newfilename");
+ob_implicit_flush(true);
+ob_start();
+
 echo "Downloading $url "; 
 echo "<br>";
+ob_flush();
+flush();
+
+$filename = str_replace ([' ','%', '?', '[' , ']', '-','&', '^', '*' , '/'], '', $filename);
+file_put_contents("downloads/$filename", fopen("$url", 'r'), LOCK_EX);
+echo "Downloaded, correcting name <br>";
+ob_flush();
+flush();
+include 'filetype.php';
+rename("files/$filename", "downloads/$newfilename");
+
 echo "to ";
 echo "<br>";
 $downloadedto = 'http://' . $domain . '/downloads/' . $newfilename;
 
 echo "<a href='$downloadedto'>$downloadedto</a>";
+ob_flush();
+flush();
 
 if(!empty($email)){
 	mail ($email, "Your cloud download is complete!", "Good news! Your cloud download of $filename is finished! please download it soon before it is auto deleted from our server $downloadedto");
